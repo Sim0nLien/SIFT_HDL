c03de1c3daa30a3284005c71fe6d61e7dcf45f04
Sim0nLien <simon.lienard1@gmail.com>
2025-08-29 16:20:09 +0200

    Modification de derivate Ixy afin de séparer les deux valids

--- Src/top_Harris_matrice/derivate_products/derivate_Ixy.vhd
+++ Src/top_Harris_matrice/derivate_products/derivate_Ixy.vhd
@@ -4,69 +4,77 @@ use IEEE.numeric_std.all;
 
 entity derivate_Ixy is
     port (
-        clk         : in  std_logic;
-        reset       : in  std_logic;
-        valid_in    : in  std_logic;
+        clk           : in  std_logic;
+        reset        : in  std_logic;
+        valid_in_x   : in  std_logic;
+        valid_in_y   : in  std_logic;
         Ix1          : in  std_logic_vector(12 downto 0);
         Ix2          : in  std_logic_vector(12 downto 0);
         Ix3          : in  std_logic_vector(12 downto 0);
         Iy1          : in  std_logic_vector(12 downto 0);
         Iy2          : in  std_logic_vector(12 downto 0);
         Iy3          : in  std_logic_vector(12 downto 0);
-        valid_out   : out std_logic;
-        Ixy1        : out std_logic_vector(15 downto 0); -- On travail avec 16 bits à partir de maintenant
-        Ixy2        : out std_logic_vector(15 downto 0);
-        Ixy3        : out std_logic_vector(15 downto 0)
+        valid_out    : out std_logic;
+        Ixy1         : out std_logic_vector(15 downto 0); -- On travail avec 16 bits à partir de maintenant
+        Ixy2         : out std_logic_vector(15 downto 0);
+        Ixy3         : out std_logic_vector(15 downto 0)
     );
 
 end derivate_Ixy;
 
 architecture Behavioral of derivate_Ixy is
 
-    type state_type is (INIT, WAITING, COMPUTE, OUTPUT);
-    signal state : state_type := INIT;
+    type state_type is (WAITING, COMPUTE, OUTPUT);
+    signal state : state_type := WAITING;
 
     signal Ix1_reg, Ix2_reg, Ix3_reg : signed(12 downto 0) := (others => '0');
     signal Iy1_reg, Iy2_reg, Iy3_reg : signed(12 downto 0) := (others => '0');
     signal Ixy1_reg, Ixy2_reg, Ixy3_reg : unsigned(25 downto 0) := (others => '0');
 
-begin 
+    signal flag_X : std_logic := '0';
+    signal flag_Y : std_logic := '0';
+
+begin
 
     process(clk)
         begin
             if rising_edge(clk) then
                 if reset = '1' then
-                    state <= INIT;
+                    state <= WAITING;
                     valid_out <= '0';
                     Ixy1_reg <= (others => '0');
                     Ixy2_reg <= (others => '0');
                     Ixy3_reg <= (others => '0');
+                    flag_X <= '0';
+                    flag_Y <= '0';
 
                 else
                     case state is
-                        when INIT =>
-                            valid_out <= '0';
-                            Ixy1_reg <= (others => '0');
-                            Ixy2_reg <= (others => '0');
-                            Ixy3_reg <= (others => '0');
-                            state <= WAITING;
 
                         when WAITING =>
                             valid_out <= '0';
-                            if valid_in = '1' then
+                            if valid_in_x = '1' then
                                 Ix1_reg <= signed(Ix1);
                                 Ix2_reg <= signed(Ix2);
                                 Ix3_reg <= signed(Ix3);
+                                flag_X <= '1';
+                            end if;
+                            if valid_in_y = '1' then
                                 Iy1_reg <= signed(Iy1);
                                 Iy2_reg <= signed(Iy2);
                                 Iy3_reg <= signed(Iy3);
-                                valid_out <= '0';
+                                flag_Y <= '1';
+                            end if;
+
+                            if flag_X = '1' and flag_Y = '1' then
                                 state <= COMPUTE;
                             end if;
 
                             -- TODO a vérifier je suis pas sûr pour la taille
                         
                         when COMPUTE =>
+                            flag_X <= '0';
+                            flag_Y <= '0';
                             Ixy1_reg <= unsigned(Ix1_reg) * unsigned(Iy1_reg);
                             Ixy2_reg <= unsigned(Ix2_reg) * unsigned(Iy2_reg);
                             Ixy3_reg <= unsigned(Ix3_reg) * unsigned(Iy3_reg);
---
+++ Src/top_Harris_matrice/top_harris_matrice.vhd
@@ -0,0 +1,11 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity top_harris_matrice is
+    port(
+        clk            : in std_logic;
+        reset          : in std_logic;
+        valid_in_Ixx   : in std
+end top_harris_matrice;
+
