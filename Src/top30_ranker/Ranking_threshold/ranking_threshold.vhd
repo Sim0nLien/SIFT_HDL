646cf3074254cb35af7f2ef9f4d2a19459a90f54
Sim0nLien <simon.lienard1@gmail.com>
2025-09-01 17:47:19 +0200

    Ranking_threshold fini
    - Simplification du module avec ajout d'une hypothèse
    - Vérification du testbench

--- Src/top30_ranker/Ranking_threshold/ranking_threshold.vhd
+++ Src/top30_ranker/Ranking_threshold/ranking_threshold.vhd
@@ -2,6 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
+-- In this bloc, I consider the new_threshold_time faster than the data arrival time
 
 entity ranking_threshold is
     port (
@@ -11,13 +12,14 @@ entity ranking_threshold is
         valid_threshold : in  STD_LOGIC;
         data_in         : in  STD_LOGIC_VECTOR(15 downto 0);
         threshold       : in  STD_LOGIC_VECTOR(15 downto 0);
+        end_comparing   : out  STD_LOGIC;
         valid_out       : out STD_LOGIC;
         data_out        : out STD_LOGIC_VECTOR(15 downto 0)
     );
 end ranking_threshold;
 
 architecture Behavioral of ranking_threshold is
-begin
+
 
 
     type state_type is (INIT, WAITING, COMPARE, UPDATE);
@@ -25,6 +27,8 @@ begin
 
     signal data_reg : signed(15 downto 0) := (others => '0');
     signal threshold_reg : signed(15 downto 0) := (others => '0');
+    
+    signal flag : std_logic;
 
 begin 
 
@@ -37,6 +41,8 @@ begin
                 threshold_reg <= (others => '0');
                 valid_out     <= '0';
                 data_out      <= (others => '0');
+                end_comparing <= '0';
+                flag          <= '0';
 
             else
                 case state is
@@ -46,32 +52,36 @@ begin
                         valid_out     <= '0';
                         data_out      <= (others => '0');
                         state         <= WAITING;
+                        end_comparing <= '0';
+                        flag          <= '0';
+
 
                     when WAITING =>
                         valid_out <= '0';
+                        end_comparing <= '0';
+                        if valid_in = '1' then 
+                            state <= COMPARE;
+                            data_reg <= signed(data_in);
+                            flag <= '1';
+                        end if;
                         if valid_threshold = '1' then
                             state <= UPDATE;
-                        else if valid_in = '1' then 
-                            state <= COMPARE;
                         end if;
                     
                     when COMPARE =>
-                        if valid_threshold = '1' then
-                            state <= UPDATE;
-                            flag <= '1'; -- In the case new threshold while comparing
+                        if data_reg > threshold_reg then
+                            data_out  <= std_logic_vector(data_reg);
+                            valid_out <= '1';
                         else
-                            if data_reg > threshold_reg then
-                                data_out  <= std_logic_vector(data_reg);
-                                valid_out <= '1';
-                            else
-                                data_out  <= (others => '0');
-                                valid_out <= '0';
-                            end if;
-                            state <= WAITING;
+                            data_out  <= (others => '0');
+                            valid_out <= '0';
                         end if;
+                        state <= WAITING;
+                        end_comparing <= '1';
                         
                     when UPDATE =>
                         threshold_reg <= signed(threshold);
+                        state <= WAITING;
                         if flag = '1' then
                             state <= COMPARE;
                             flag <= '0';
