6c0bb62375042e7b038c25e44904dd445831ecac
Sim0nLien <simon.lienard1@gmail.com>
2025-09-05 00:05:26 +0200

    Ecriture du read :
    - Ecriture du read, cependant je ne suis pas sur du tout il doit etre testé et ajusté
    - Début de l'écriture de la spécification de la partie ram
    -  Changement du nom de la memoire
    - Début écriture du block 3x3 à réflichir

--- Docs/Ram/Ram_Specifications.md
+++ Docs/Ram/Ram_Specifications.md
@@ -1,3 +1,28 @@
-# RAM Specification
+# RAM Specification (Draft Version v0.1)
+
 
 ## 1. Introduction
+
+This documents describe the functional specifications of the RAM module used in the SIFT FPGA implementation. 
+
+
+### 1.1 Functional Overview
+
+This module is designed to store image data. The manager module requests a data address, and this module provides the value at that address along with the value directly below it. The RAM is dual-port, allowing the next row of the image to be written while computations are performed simultaneously. Finally, the 3×3 memory stores data before carrying out the convolution calculation.
+
+## 2. Dual Port RAM
+
+
+## 3. Write Module
+
+
+## 4. Ram Module
+
+
+## 5. Read Module
+
+
+## 6. 3*3 Memory Module
+
+
+## 7. Top RAM Module
--- Src/Ram/read_image/read_image.vhd
+++ Src/Ram/read_image/read_image.vhd
@@ -4,30 +4,25 @@ use IEEE.NUMERIC_STD.all;
 
 entity read_image is
     port(
-        clk         : in  std_logic;
-        reset       : in  std_logic;
-        valid_in    : in std_logic;
-        data_in     : in std_logic_vector(18 downto 0);
-        valid_out   : out std_logic;
-        data_out_1  : out std_logic_vector(8 downto 0);
-        data_out_2  : out std_logic_vector(8 downto 0);
-        data_out_3  : out std_logic_vector(8 downto 0)
+        clk             : in  std_logic;
+        reset           : in  std_logic;
+        valid_ram_in    : in std_logic;
+        valid_manager_in: in std_logic;
+        addr_in         : in std_logic_vector(18 downto 0);
+        data_in         : in std_logic_vector(8 downto 0);
+        valid_ram_out   : out std_logic;
+        valid_mem_out   : out std_logic;
+        addr_out        : out std_logic_vector(18 downto 0);
+        data_out        : out std_logic_vector(8 downto 0)
     );
 end read_image;
   
 architecture Behavioral of read_image is
-    type state_type is (INIT, WAITING, OUTPUT_1, OUTPUT_2, OUTPUT_3);
+    
+    type state_type is (INIT, WAITING, SEND_REQUEST, SEND_DATA);
     signal state : state_type := INIT;
 
-    signal addr_pix_1 : unsigned(18 downto 0);
-    signal addr_pix_2 : unsigned(18 downto 0);
-    signal addr_pix_3 : unsigned(18 downto 0);
-
-    -- I don't know if that will be usefull but we never know
-
-    signal data_pix_1 : unsigned(8 downto 0);
-    signal data_pix_2 : unsigned(8 downto 0);
-    signal data_pix_3 : unsigned(8 downto 0);
+    signal count : integer := 0;
 
 begin
 
@@ -37,32 +32,50 @@ begin
             if reset = '1' then
                 state <= INIT;
                 valid_out <= '0';
-                addr_pix_1 <= (others => '0');
-                addr_pix_2 <= (others => '0');
-                addr_pix_3 <= (others => '0');
-                data_pix_1 <= (others => '0');
-                data_pix_2 <= (others => '0');
-                data_pix_3 <= (others => '0');
+                addr_out <= (others => '0');
+                data_out <= (others => '0');
+                count <= 0;
 
             else
                 case state is
                     when INIT =>
                         valid_out <= '0';
-                        addr_pix_1 <= (others => '0');
-                        addr_pix_2 <= (others => '0');
-                        addr_pix_3 <= (others => '0');
-                        data_pix_1 <= (others => '0');
-                        data_pix_2 <= (others => '0');
-                        data_pix_3 <= (others => '0');
                         state <= WAITING;
+                        count <= 0;
 
                     when WAITING =>
-                        valid_out <= '0';
-                        if valid_in = '1' then
+                        valid_mem_out <= '0';
+                        count <= 0;
+                        if valid_manager_in = '1' then
                            state <= OUTPUT_1;
                            pixel_1 <= unsigned(data_in);
-                           addr_pix_1 <= addr_pix_1 + 1;
+                           addr_pix <= addr_in - 512;
+                        end if;
 
+                    when SEND_REQUEST =>
+                        valid_ram_out <= '1';
+                        addr_out <= std_logic_vector(addr_pix);
+                        state <= SEND_DATA;
+
+                    when SEND_DATA =>
+                        valid_mem_out <= '1';
+                        if valid_ram_in = '1' then
+                            data_out <= data_in;
+                            addr_pix <= addr_pix + 512;
+                            state <= SEND_REQUEST;
+                            count <= count + 1;
+                            if count = 2 then
+                                state <= WAITING;
+                            end if;
+                        end if;
+                    end case;
+            end if;
+        end if;
+    end process;
+end Behavioral;
+
+
+                            
 
 
 
