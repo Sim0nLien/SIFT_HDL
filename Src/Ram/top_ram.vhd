8f8524c9d077c362cd5c49640420350229652b6e
Sim0nLien <simon.lienard1@gmail.com>
2025-09-07 23:18:53 +0200

    Top_ram réalisé
    - Réalisation du bloc top ram
    - Cependant je dois encore réaliser les tests de ce bloc

--- Src/Ram/top_ram.vhd
+++ Src/Ram/top_ram.vhd
@@ -4,26 +4,68 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity top_ram is
     port(
-        clk         : in  std_logic;
-        reset       : in  std_logic;
-        valid_write : in  std_logic;
-        data_write  : in std_logic_vector(8 downto 0);
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        )
+        clk           : in  std_logic;
+        reset         : in  std_logic;
+        valid_write   : in  std_logic;
+        valid_manager : in  std_logic;
+        data_write    : in  std_logic_vector(7 downto 0);
+        valid_out     : out std_logic;
+        data_out      : out std_logic_vector(7 downto 0);
+        addr_out      : out std_logic_vector(18 downto 0)
+    );
+end architecture Behavioral;
+
+architecture Behavioral of top_ram is        
+    
+
+    signal valid_write_ram : std_logic;
+    signal valid_ram_read  : std_logic;
+    signal valid_read_ram  : std_logic;
+
+    signal data_write_ram : std_logic_vector(7 downto 0);
+    signal data_ram_read  : std_logic_vector(7 downto 0);
+
+    signal addr_write_ram : std_logic_vector(18 downto 0);
+    signal addr_ram_read  : std_logic_vector(18 downto 0);
+
+begin
+
+    write_image_inst : entity work.write_image
+        port map(
+               clk => clk,
+               reset => reset,
+               valid_write => valid_write,
+               valid_out => valid_ram_read,
+               data_out => data_write_ram
+               addr_out => addr_write_ram
+        );
+
+    dual_port_ram_inst : entity work.dual_port_ram
+        generic map(
+                DATA_WIDTH => 8,
+                ADDR_WIDTH => 18
+        );
+        port map(
+                clk => clk,
+                valid_write => valid_write_ram,
+                valid_read => valid_read_ram,
+                addr_write => addr_write_ram,
+                data_write => data_write_ram,
+                addr_read => addr_ram_read,
+                data_read => data_ram_read
+        );
+
+    read_image_inst : entity work.read_image
+        port map(
+                clk => clk,
+                reset => reset,
+                valid_ram_in => valid_ram_read,
+                valid_manager_in => valid_out,
+                addr_ram_read => addr_ram_read,
+                data_ram_read => data_ram_read,
+                valid_ram_out => valid_ram_read,
+                valid_mem_out => valid_out,
+                addr_out => addr_out,
+                data_out => data_out
+        );
+end Behavioral;
